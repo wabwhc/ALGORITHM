5be164a34fa3a4e4cc1eda97355e082bae650222
test <testtest123@naver.com>
2023-01-30 20:41:53 +0900

    add number of question

--- DataStructure/AbsoluteValueHeap.java
+++ DataStructure/AbsoluteValueHeap.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
+//11286
 public class AbsoluteValueHeap {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- DataStructure/Bestseller.java
+++ DataStructure/Bestseller.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.TreeMap;
 
+//1302
 public class Bestseller {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
